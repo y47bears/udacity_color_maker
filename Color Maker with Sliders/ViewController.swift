552546d1cc49f7b71b9e7654ec27d15d9284b2dd
D <dslee47@gmail.com>
2018-05-14 10:28:04 -0700

    Initial Commit

---
+++ Color Maker with Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Color Maker with Sliders
+//
+//  Created by Daniel Lee on 5/14/18.
+//  Copyright © 2018 Daniel Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
